438a2e7053d6cd3a40099e7850dec01066fe8937
zkvKirov <zkkirov@yandex.ru>
2022-06-22 19:58:57 +0300

    новый токен

--- src/main/kotlin/ru/netology/pusher/main.kt
+++ src/main/kotlin/ru/netology/pusher/main.kt
@@ -23,7 +23,7 @@ fun main() {
           "postId": 2,
           "postAuthor": "Netology"
         }""".trimIndent())
-        .setToken("eif1lblGTrqx4Wq1DON3Be:APA91bHT4TCIhp58FppM0OH7ymqAumfpPsOXEkkE2tcZpE2hNr6WCtfBaOX1t8_GQGvD5ecmQ1NbD9993vGPezwidr5ldN3zjXNVT_bTl8nM2ObL6ZnPr59xpeqgxLYcnbjD8u-4ViOz")
+        .setToken("faYbegiQSvuCQsQRD6lk86:APA91bGZ24EGhuAQUNElNlKMSIFocDCL7MDsOMwsY-wfVyYYCU3l1CgxzYZiYppoKse3ZHeMtScxDOxKnuy5MEy5RZWCvjwThEpHPB5IfzSEkaMIq3SnX3UNlUOXjNGQLsjut6QwCnqO")
         .build()
 
     FirebaseMessaging.getInstance().send(message)
